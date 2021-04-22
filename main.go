8c54a3afd6e58b11bf0f30ea88522c9b69749f24
Hariz Sufyan Munawar <munawarhariz@gmail.com>
2021-04-22 12:56:40 +0700

    Create a function for generating a new deck of cards

--- main.go
+++ main.go
@@ -9,4 +9,20 @@ func main() {
 	fmt.Println("Hello World")
 
 	fmt.Println(card.Card{Suit: card.Heart, Rank: card.Queen}.Stringify())
+	deck := CreateDeck()
+
+	fmt.Println(deck)
+	fmt.Println(len(deck))
+}
+
+func CreateDeck() []card.Card {
+	var cards []card.Card
+
+	for _, suit := range [...]card.Suit{card.Spade, card.Diamond, card.Clover, card.Heart} {
+		for rank := card.Ace; rank <= card.King; rank++ {
+			cards = append(cards, card.Card{Suit: suit, Rank: rank})
+		}
+	}
+
+	return cards
 }
